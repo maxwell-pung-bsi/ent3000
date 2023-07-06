862885fc6f2d8834aaccea24f07cfa79998a10ca
maxwell-pung-bsi <mpung@bluesages.com>
2023-07-06 08:56:53 -0500

    Output results as JSON to a file by passing -j flag

--- src/com/reallyreallyrandom/ent3000/Suite.java
+++ src/com/reallyreallyrandom/ent3000/Suite.java
@@ -24,12 +24,18 @@ SOFTWARE.
 
 package com.reallyreallyrandom.ent3000;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.random.RandomGenerator;
+
+import org.json.simple.JSONObject;
 
 import com.reallyreallyrandom.ent3000.thetests.Chi;
 import com.reallyreallyrandom.ent3000.thetests.Compression;
@@ -47,12 +53,14 @@ public class Suite {
     final static int[] ACCEPTABLE_SAMPLE_SIZES = { 25_000, 50_000, 75_000, 100_000, 150_000, 200_000, 300_000, 400_000,
             500_000, 600_000,
             700_000, 800_000, 900_000, 1_000_000 };
+    private static boolean isJsonOutputEnabled = false;
 
     // TODO Use library like https://picocli.info/ for arguments handling.
     public static void main(String[] args) {
         System.out.println("\nent3000 starting...");
         System.out.println("--help option to display this help.");
         String inFile = null;
+        String outFile = null;
 
         try {
             if (args.length > 0) {
@@ -63,11 +71,33 @@ public class Suite {
                     System.out.println(help);
                     System.exit(0);
                 }
+                if (args[0].equals("-j")) {
+                    isJsonOutputEnabled = true;
+                    System.out.println("JSON output enabled.");
+                    if (args.length > 1)
+                    {
+                        inFile = args[1];
+                        if (args.length < 3)
+                        {
+                            throw new Exception("No output file provided.");
+                        }
+                        else
+                        {
+                            outFile = args[2];
+                        }
+                    }
+                    else
+                    {
+                        inFile = "internal_CSPRNG";
+                        outFile = "internal_CSPRNG_results.json";
+                    }
+                    System.out.println("JSON output path: " + outFile);
+                }
             } else {
                 inFile = "internal_CSPRNG";
             }
             Suite ent = new Suite();
-            ent.runTests(inFile);
+            ent.runTests(inFile, outFile);
         } catch (Exception e) {
             System.err.println("Problem testing file: " + inFile);
             System.err.println(e.getMessage());
@@ -107,11 +137,12 @@ public class Suite {
         return truncated;
     }
 
-    public void runTests(String filename) throws IOException, NoSuchAlgorithmException {
+    public void runTests(String inputFilename, String outputFilename) throws IOException, NoSuchAlgorithmException {
         // TODO Break this out into a method?
         byte[] samples;
-
-        if (filename == "internal_CSPRNG") {
+        HashMap<String, Object> runResult = new HashMap<String, Object>();
+        ArrayList<Object> testResults = new ArrayList<Object>();
+        if (inputFilename == "internal_CSPRNG") {
             RandomGenerator rng = SecureRandom.getInstance("NativePRNG");
             int noFakeSamples = ACCEPTABLE_SAMPLE_SIZES[rng.nextInt(ACCEPTABLE_SAMPLE_SIZES.length)];
             samples = new byte[noFakeSamples];
@@ -119,80 +150,154 @@ public class Suite {
             System.out.println("Testing internal native CSPRNG.");
             System.out.println("Testing " + samples.length + " bytes.");
         } else {
-            samples = Files.readAllBytes(Paths.get(filename));
+            Path inputFilePath = Paths.get(inputFilename);
+            samples = Files.readAllBytes(inputFilePath);
             samples = truncate(samples);
             System.out.println("Testing first " + samples.length + " bytes.");
+            runResult.put("Filename", inputFilePath.getFileName().toString());
         }
 
         ITestish test;
         double pValue;
         String testComment = null;
-
+        runResult.put("BytesTested", samples.length);
         test = new Sanity();
         pValue = test.getPValue(samples);
         if (pValue > SANITY_ALPHA) {
+            if (isJsonOutputEnabled) {
+                runResult.put("Sane", true);
+            }
             System.out.println("Sane sample file. Good.");
             System.out.println("------------------------------------");
         } else {
+            if (isJsonOutputEnabled) {
+                runResult.put("Sane", false);
+                JSONObject runResultJsonObject = new JSONObject(runResult);
+                FileWriter file = new FileWriter(outputFilename);
+                file.write(runResultJsonObject.toJSONString());
+                file.close();
+            }
             System.out.println("Insane sample file. Bad.");
             System.out.println("Exit.");
             System.exit(-1);
         }
 
+        HashMap<String, Object> entropyTestResult = new HashMap<String, Object>();
+        Boolean isTestPass = false;
         test = new Entropy();
         pValue = test.getPValue(samples);
         if (pValue > ALPHA) {
             testComment = "PASS.";
+            isTestPass = true;
         } else {
             testComment = "FAIL.";
+            isTestPass = false;
+        }
+        if (isJsonOutputEnabled) {
+            entropyTestResult.put("Name", "Entropy");
+            entropyTestResult.put("PValue", pValue);
+            entropyTestResult.put("Pass", isTestPass);
+            testResults.add(entropyTestResult);
         }
         prettyPrintResult("Entropy", pValue, testComment);
-
+        
+        HashMap<String, Object> compressionTestResult = new HashMap<String, Object>();
         test = new Compression();
         pValue = test.getPValue(samples);
         if (pValue > ALPHA) {
             testComment = "PASS.";
+            isTestPass = true;
         } else {
             testComment = "FAIL.";
+            isTestPass = false;
+        }
+        if (isJsonOutputEnabled) {
+            compressionTestResult.put("Name", "Compression");
+            compressionTestResult.put("PValue", pValue);
+            compressionTestResult.put("Pass", isTestPass);
+            testResults.add(compressionTestResult);
         }
         prettyPrintResult("Compression", pValue, testComment);
 
+        HashMap<String, Object> chiTestResult = new HashMap<String, Object>();
         test = new Chi();
         pValue = test.getPValue(samples);
         if (pValue > ALPHA) {
             testComment = "PASS.";
+            isTestPass = true;
         } else {
             testComment = "FAIL.";
+            isTestPass = false;
+        }
+        if (isJsonOutputEnabled) {
+            chiTestResult.put("Name", "Chi");
+            chiTestResult.put("PValue", pValue);
+            chiTestResult.put("Pass", isTestPass);
+            testResults.add(chiTestResult);
         }
         prettyPrintResult("Chi", pValue, testComment);
-
+        
+        HashMap<String, Object> meanTestResult = new HashMap<String, Object>();
         test = new Mean();
         pValue = test.getPValue(samples);
         if (pValue > ALPHA) {
             testComment = "PASS.";
+            isTestPass = true;
         } else {
             testComment = "FAIL.";
+            isTestPass = false;
+        }
+        if (isJsonOutputEnabled) {
+            meanTestResult.put("Name", "Mean");
+            meanTestResult.put("PValue", pValue);
+            meanTestResult.put("Pass", isTestPass);
+            testResults.add(meanTestResult);
         }
         prettyPrintResult("Mean", pValue, testComment);
 
+        HashMap<String, Object> piTestResult = new HashMap<String, Object>();
         test = new Pi();
         pValue = test.getPValue(samples);
         if (pValue > ALPHA) {
             testComment = "PASS.";
+            isTestPass = true;
         } else {
             testComment = "FAIL.";
+            isTestPass = false;
+        }
+        if (isJsonOutputEnabled) {
+            piTestResult.put("Name", "Pi");
+            piTestResult.put("PValue", pValue);
+            piTestResult.put("Pass", isTestPass);
+            testResults.add(piTestResult);
         }
         prettyPrintResult("Pi", pValue, testComment);
-
+        
+        HashMap<String, Object> uncorrelationTestResult = new HashMap<String, Object>();
         test = new UnCorrelation();
         pValue = test.getPValue(samples);
         if (pValue > ALPHA) {
             testComment = "PASS.";
+            isTestPass = true;
         } else {
             testComment = "FAIL.";
+            isTestPass = false;
+        }
+        if (isJsonOutputEnabled) {
+            uncorrelationTestResult.put("Name", "UnCorrelation");
+            uncorrelationTestResult.put("PValue", pValue);
+            uncorrelationTestResult.put("Pass", isTestPass);
+            testResults.add(uncorrelationTestResult);
         }
         prettyPrintResult("UnCorrelation", pValue, testComment);
 
+        if (isJsonOutputEnabled) {
+            runResult.put("Tests", testResults);
+            JSONObject runResultJsonObject = new JSONObject(runResult);
+            FileWriter file = new FileWriter(outputFilename);
+            file.write(runResultJsonObject.toJSONString());
+            file.close();
+        }
         System.out.println("------------------------------------");
         System.out.println("Finished.");
     }
